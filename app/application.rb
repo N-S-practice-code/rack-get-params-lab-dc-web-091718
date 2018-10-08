cbfd47433d911a321645664b0599d52916f83d57
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-08 13:39:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
