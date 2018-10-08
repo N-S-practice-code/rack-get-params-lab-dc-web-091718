91aee6702d71c597ac9ede892558ea73ad1777c1
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-08 13:41:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-      item = req.params["q"]
+      item = req.params["item"]
       resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
